688767108d8f8d555d31a0421c0af025f3568442
Erik Jones <jonzee1914@gmail.com>
2021-12-05 18:45:49 -0500

    added license types

---
+++ Develop/utils/licensesTypes.js
@@ -0,0 +1,47 @@
+module.exports = licenses = [
+    {
+      licenseName: "None",
+      licenseBadge:
+        "",
+    },
+    {
+      licenseName: "GNU Affero General Public License v3.0",
+      licenseBadge:
+        "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
+    },
+    {
+      licenseName: "GNU General Public License v3.0",
+      licenseBadge:
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    },
+    {
+      licenseName: "GNU Lesser General Public License v3.0",
+      licenseBadge:
+        "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
+    },
+    {
+      licenseName: "Mozilla Public License 2.0",
+      licenseBadge:
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    },
+    {
+      licenseName: "Apache license 2.0",
+      licenseBadge:
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    },
+    {
+      licenseName: "MIT",
+      licenseBadge:
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    },
+    {
+      licenseName: "Boost Software License 1.0",
+      licenseBadge:
+        "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)",
+    },
+    {
+      licenseName: "The Unlicense",
+      licenseBadge:
+        "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    },
+  ];
