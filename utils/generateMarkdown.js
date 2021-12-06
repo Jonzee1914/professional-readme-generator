5b6b062bf99aab329975c6d47d99047d37b75515
Erik Jones <jonzee1914@gmail.com>
2021-12-06 00:02:34 -0500

    wrote generate functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,117 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Functions to return a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenses = require("./licenses.js");
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(licenses, licenseName){
+  if(licenseName === "None") {
+    return "";
+  }
+  let index = licenses.findIndex( license => license.licenseName == licenseName)
+  let licenseBadge = licenses[index].licenseBadge
+  return licenseBadge
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license){
+    if(license === "None"){
+    return "";
+  }
+  return `* [License](#license)`
+}
+
+// Functions to return link for other "when" sections
+const installCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Installation](#installation)`
+  }
+}
+const contributeCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Contributions](#contributions)`
+  }
+}
+const testCheck = check => {
+  if (!check) {
+    return '';
+  } else {
+    return `* [Testing](#testing)`
+  }
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if(license === "None"){
+    return "";
+  }
+  return `## License
+  This project is covered under the ${license} License.`
+}
+
+// Functions that return the when base sections
+const renderInstall = installText => {
+  if (!installText) {
+    return ''
+  } else {
+    return `## Installation
+  ${installText}
+  `
+  }
+}
+const renderContribute = contributeText => {
+  if (!contributeText) {
+    return ''
+  } else {
+    return `## Contributions
+  ${contributeText}
+  `
+  }
+}
+const renderTesting = testText => {
+  if (!testText) {
+    return ''
+  } else {
+    return `## Testing
+  ${testText}
+  `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+${renderLicenseBadge(licenses,data.license)}
+## Description 
+${data.description}
+## Table of Contents
+${installCheck(data.install)}
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseLink(data.license)}
+${contributeCheck(data.contribution)}
+${testCheck(data.testing)}
+* [Questions](#questions)
+## Installation
+${renderInstall(data.install)}
+## Usage 
+${data.usage}
+## Credits
+${data.credits}
+${renderLicenseSection(data.license)}
+## Contributions
+${renderContribute(data.contribution)}
+## Tests
+${renderTesting(data.testing)}
+## Questions
+Any questions, checkout my [Github profile](https://github.com/${data.github})
+or email me @ [${data.email}](mailto:${data.email})
 `;
 }
+
 
 module.exports = generateMarkdown;
